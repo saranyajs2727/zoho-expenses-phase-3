2ed2d30039db6a00b468a6262db6e51ba1428d05
saranyajs2727 <83491114+saranyajs2727@users.noreply.github.com>
2021-11-28 16:29:38 +0530

    Update gpay.js

--- front-end/pages/gpay.js
+++ front-end/pages/gpay.js
@@ -1,18 +1,18 @@
 
-import Gpay from "../components/Gpay";
-import Head from "next/head";
-import Layout from "../components/Layout";
-import withAuth from "../auth/prodectedroutes";
-function home() {
-  return (
-    <div>
-      <Layout />
-      <Head> 
-        <title>payment Page </title>
-      </Head>
+// import Gpay from "../components/Gpay";
+// import Head from "next/head";
+// import Layout from "../components/Layout";
+// import withAuth from "../auth/prodectedroutes";
+// function home() {
+//   return (
+//     <div>
+//       <Layout />
+//       <Head> 
+//         <title>payment Page </title>
+//       </Head>
 
-      <Gpay />
-    </div>
-  );
-}
-export default withAuth(home);
+//       <Gpay />
+//     </div>
+//   );
+// }
+// export default withAuth(home);
